4fb054067eb5da9ba59768b5e9ff6be3db8948da
get-softwares <getsoftwares18@gmail.com>
2021-09-05 22:13:29 +0530

    hi

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/getemail", async (req, res) => {
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'getsoftwares18@gmail.com',
+                user: 'getsoftwares18 @gmail.com',
                 pass: 'aayushkumarjha@drf'
             }
         });
