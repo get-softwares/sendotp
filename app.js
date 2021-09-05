67c6fa971c3ea60937825ede7022cfd0eefbdab7
get-softwares <getsoftwares18@gmail.com>
2021-09-05 19:18:06 +0530

    hi

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const nodemailer = require("nodemailer")
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.post("/getemail", async (req, res) => {
+    try {
+        let hello = Math.floor(100000 + Math.random() * 900000)
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'deeptamresearchfoundationweb@gmail.com',
+                pass: 'aayushkumarjha@drf'
+            }
+        });
+
+        var mailOptions = {
+            from: 'deeptamresearchfoundation@gmail.com',
+            to: req.body.email,
+            subject: `OTP for registeration is ${hello}`,
+            text: `OTP for registeration is ${hello}`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                res.send(error).status(404)
+            } else {
+                res.json({ otp: hello }).status(200);
+            }
+        });
+    }
+    catch {
+
+    }
+})
+
+app.listen(port, () => {
+})
